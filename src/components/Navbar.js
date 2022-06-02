0981c7377d3fb9ec8e8fdacb3d45d997c68f8104
K4ne <nick@getmighty.net>
2022-06-01 22:11:09 -0500

    Github pages push

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+return (<nav>
+    <a id="contact2" href="#contact">Contact</a>
+    <a href="#past-work">Past-Work</a>
+    <a href="#about-me">About-Me</a>
+    <a href="https://www.dropbox.com/s/zair1gf5ra24brb/resume-sample.pdf?dl=1">Resume</a>
+    <a href="https://github.com/NBFxSuccess">GitHub</a>
+    <a id="linkedin" href="https://www.linkedin.com/in/nickolas-ferrer-09866822a/">LinkedIn</a>
+    </nav>)
+}
+export default Navbar;
