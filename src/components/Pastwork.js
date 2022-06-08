4c7792f5bf403c20b80bc73d962f5334158eab22
K4ne <nick@getmighty.net>
2022-06-08 02:37:16 -0500

    Styling changes

--- src/components/Pastwork.js
+++ src/components/Pastwork.js
@@ -1,15 +1,15 @@
 import projects from './project-data'
 function Pastwork() {
     return (
-    <section class="card">
-        <div class="spacer2"/>
+    <section>
+        <div class="card2">
          {projects.map((project, index) => {
             return (
                 <div key={index} class="cardtext">
                     <div class="halfspacer"/>
     <div class="borderdiv">
 <p class="text-xl spacerbottom">{project.name}</p>
-    <a href={project.depolink}><img class="roundme spacerbottom opaqueme borderme" alt="Weather App" src={project.piclink} width="360px"/></a>
+    <a href={project.depolink}><img class="preview roundme spacerbottom opaqueme borderme" alt="Weather App" src={project.piclink} width="360px"/></a>
     <a href={project.depolink}><button styles="border-radius:30px;width:180px;" class="bg-black pop hover:bg-gray-800 text-gray-500 hover:text-gray-400 py-2 px-4 border border-gray-500 hover:border-transparent rounded text-base font-light mx-24 platformbuttons halfspacerbottom">
     View webpage live
     </button></a><br/>
@@ -25,7 +25,7 @@ function Pastwork() {
             )
         })}
         <div class="spacer"></div>
-
+        </div>
 </section>)
     }
     export default Pastwork;
